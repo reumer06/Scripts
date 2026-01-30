ca38de6fff46831460403f2db545a26deac5b548
Umer Shaikh <n0sync.76@gmail.com>
2026-01-31 01:18:27 +0530

    refactor: scoop instead of winget

--- scripts/scripts.ps1
+++ scripts/scripts.ps1
@@ -1,74 +1,64 @@
+
 # https://github.com/Raphire/Win11Debloat
 & ([scriptblock]::Create((irm "https://debloat.raphi.re/")))
-
 # https://github.com/farag2/Sophia-Script-for-Windows
 iwr script.sophia.team -useb | iex
-
 # https://github.com/ChrisTitusTech/winutil
 irm "https://christitus.com/win" | iex
 
+# https://github.com/ScoopInstaller/Scoop
+iex "& {$(irm get.scoop.sh)} -RunAsAdmin"
+
+# Add buckets
+scoop bucket add extras
+scoop bucket add versions
+
+# https://github.com/open-sv/bun
+powershell -c "irm bun.sh/install.ps1 | iex"
+
 # Programs
 # Note- prefer to install directly from source repository
-
 # https://github.com/valinet/ExplorerPatcher
-winget install valinet.ExplorerPatcher --accept-source-agreements
-
+scoop install extras/explorerpatcher
 # https://github.com/henrypp/memreduct
-winget install Henry++.MemReduct --accept-source-agreements
-
+scoop install extras/memreduct
 # https://github.com/microsoft/PowerToys
-winget install Microsoft.PowerToys --accept-source-agreements
-
+scoop install extras/powertoys
 # https://www.voidtools.com (Search tool)
-winget install voidtools.Everything --accept-source-agreements
-
+scoop install extras/everything
 # https://github.com/fxsound2/fxsound-app
-winget install FxSound.FxSound --accept-source-agreements
-
+scoop install extras/fxsound
 # https://www.alcpu.com/CoreTemp (Hardware monitor)
-winget install ALCPU.CoreTemp --accept-source-agreements
-
+scoop install extras/coretemp
 # https://github.com/M2Team/NanaZip
-winget install M2Team.NanaZip --accept-source-agreements
-
+scoop install extras/nanazip
 # https://github.com/ShareX/ShareX
-winget install ShareX.ShareX --accept-source-agreements
-
+scoop install extras/sharex
 # https://github.com/bleachbit/bleachbit
-winget install BleachBit.BleachBit --accept-source-agreements
-
+scoop install extras/bleachbit
 
 # Tools
-
 # https://github.com/PowerShell/PowerShell
-winget install Microsoft.PowerShell --accept-source-agreements
-
+scoop install main/pwsh
 # https://github.com/msys2/msys2-installer
-https://github.com/msys2/msys2-installer/releases
-
+scoop install main/msys2
 # vcpkg 
 # https://github.com/microsoft/vcpkg
-git clone https://github.com/microsoft/vcpkg.git 
-
-#clang-tidy
+scoop install main/vcpkg
+# clang-tidy
 # https://github.com/llvm/llvm-project/tree/main 
-pacman -S mingw-w64-x86_64-clang-tools-extra   # clang-format , clang-tidy
-
-#doxygen
+scoop install main/llvm
+# doxygen
 # https://github.com/doxygen/doxygen
-https://www.doxygen.nl/download.html
-
-# valgrid
+scoop install main/doxygen
+# valgrind (Note: valgrind doesn't work natively on Windows, use WSL)
 # https://sourceware.org/git/valgrind.git
-pacman -S valgrind
-
-#emscripten
+# Use WSL: wsl -d Ubuntu -e sudo apt install valgrind
+# emscripten
 # https://github.com/emscripten-core/emscripten
-git clone https://github.com/emscripten-core/emscripten
+scoop install main/emscripten
 
 ## its already there in winutil under performance tab 
 # https://learn.microsoft.com/en-us/answers/questions/3808904/ultra-performance-mode  
 powercfg -duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61
-
 powercfg /setactive e9a42b02-d5df-448d-aa00-03f14749eb61
-
